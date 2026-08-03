f67e9ca67af2a28146e64c6417f048fd8c876915
victor-rollup <catdog.studios.software@gmail.com>
2026-08-02 23:39:33 -0600

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export { vClickOutside } from './directives/click-outside.directive';
+export { vLazyLoading } from './directives/lazy-loading.directive';
+export { vRipple } from './directives/ripple.directive';
+export { vTooltip } from './directives/tooltip.directive';
+export { vCopy } from './directives/copy.directive';
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import('./assets/stylesheet.css');
+export { vClickOutside } from './directives/click-outside.directive';
+export { vLazyLoading } from './directives/lazy-loading.directive';
+export { vRipple } from './directives/ripple.directive';
+export { vTooltip } from './directives/tooltip.directive';
+export { vCopy } from './directives/copy.directive';
